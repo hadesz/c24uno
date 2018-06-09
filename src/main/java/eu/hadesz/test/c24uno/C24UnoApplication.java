c3a50dbc7b3104b175bc326cd5e3e6ae26951c5c
hadesz <hadeszka@gmail.com>
2018-06-09 12:09:54 +0200

    SpringBoot initial

---
+++ src/main/java/eu/hadesz/test/c24uno/C24UnoApplication.java
@@ -0,0 +1,12 @@
+package eu.hadesz.test.c24uno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C24UnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C24UnoApplication.class, args);
+	}
+}
